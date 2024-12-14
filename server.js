3d7dac6a7452fde56e26215cc80d7ae1166a269b
unknown <Buzz-brain>
2024-12-14 07:51:07 +0100

    first commit

--- server.js
+++ server.js
@@ -1,34 +1,4 @@
-// const express = require('express');
-// const mongoose = require('mongoose');
-// const bodyParser = require('body-parser');
-// const http = require('http');
-// const { Server } = require('socket.io');
-// const Container = require('./models/container');
-
-// const app = express();
-// const server = http.createServer(app);
-// const io = new Server(server);
-
-// const PORT = 3000;
-
-// // Middleware
-// app.use(bodyParser.urlencoded({ extended: true }));
-// app.use(bodyParser.json());
-// app.use(express.static('public'));
-// app.set('view engine', 'ejs');
-
-// // MongoDB Connection
-// mongoose.connect('mongodb+srv://chinomsochristian03:ahYZxLh5loYrfgss@cluster0.dmkcl.mongodb.net/buzzsecure?retryWrites=true&w=majority').then(() => console.log('Database Connected'))
-//   .catch((err) => console.error('Database Connection Error:', err));
-
-// // Routes
-// app.use('/', require('./routes/index')(io));
-
-// // Start Server
-// server.listen(PORT, () => console.log(`Server running on http://localhost:${PORT}`));
-
-
-const express = require('express');
+sconst express = require('express');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
 const http = require('http');
@@ -46,6 +16,7 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 app.use(express.static('public'));
 app.set('view engine', 'ejs');
+server.set("views", path.join(__dirname, "views")); // Set views directory
 
 // MongoDB Connection
 mongoose.connect('mongodb+srv://chinomsochristian03:ahYZxLh5loYrfgss@cluster0.dmkcl.mongodb.net/buzzsecure?retryWrites=true&w=majority')
